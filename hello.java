afcc0a34932159cd6124b5048dfc35dee77600b5
Shilpi <shilpi.tanwar@gmail.com>
2021-07-14 02:24:21 +0000

    resolving conflict after git stash

--- hello.java
+++ hello.java
@@ -2,7 +2,9 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("Making changes in remote repository and commiting");
+
+System.out.println("Making Changes thinking i am in master branch but accidentally was in child branch");
+
 }
 }
 
